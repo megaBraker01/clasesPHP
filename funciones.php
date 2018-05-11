7b9cc32572a7859aac10b1a0fb519782d9699c05
megaBraker01 <34286851+megaBraker01@users.noreply.github.com>
2018-05-11 14:59:15 +0200

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -15,13 +15,38 @@
 	echo $numero; // en este caso $numero pasa a valer 4 porque es pasado por referencia
 
 
-// FUNSION CON PARAMETROS POR DEFECTO,
-	function capitalizar($caracteres, $todoMayuscula = FALSE){ // en este caso el 2do parametro es opcional
+// FUNSION CON PARAMETROS POR DEFECTO (ESCALABLES),
+// NOTA: El valor predeterminado tiene que ser una expresión constante (NO una variable), un miembro de una clase o una llamada a una función.
+// Los valores predeterminados van siempre a la derecha de los no predeterminados
+	function capitalizar($caracteres, $todoMayuscula = FALSE, $todoMinuscula = TRUE){ // en este caso el 2do y 3er parametro son opcionales
 		$caracteres = ucwords($caracteres);
 		if($todoMayuscula){
 			$caracteres = strtoupper($caracteres);
 		}
 		return $caracteres;
 	}
+
+
+// TIPADO DE PARAMETROS
+// NOTA: en PHP el tipado de parametros se comporta practicamente como el cast
+// ya que float TRUE devuelve 1, int 2.568 devuelve 2, string 692 devuelve "692", bool 3 devuelve TRUE
+	function contatenar(float $num1, int $num2, string $var3, bool $var4, array $var5){
+		return var_dump($num1, $num2, $var3, $var4, $var5);
+	}
+
+// FUNDION VARIÁDICA
+// NOTA: Una función variádica es una función de aridad indefinida, es decir, que acepta una cantidad de argumentos variable
+// la aridad es el número de argumentos necesarios para que dicho operador o función se pueda calcular
+	function suma() {
+	    $cuenta = 0;
+	    foreach(func_get_args() as $numero){ // func_get_args funcion de PHP que obtiene todos los argumentos que se estan pasando por nuestra funcion y los devuelve como array
+		$cuenta += $numero;
+	    }
+	    return $cuenta;
+	}
+
+	echo suma(2, 5, 8); // Devuelve 15
+
+
 
 ?>
